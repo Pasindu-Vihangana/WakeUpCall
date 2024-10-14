6433f07f99128ba76d69e44d6336b2c3fb408581
Pasindu Vihangana <pasi1028@gmail.com>
2024-10-14 19:56:29 +0530

    Initial Commit

---
+++ WakeUpCall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WakeUpCall
+//
+//  Created by Pasindu Vihangana on 2024-10-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WakeUpCall/WakeUpCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeUpCallApp.swift
+//  WakeUpCall
+//
+//  Created by Pasindu Vihangana on 2024-10-14.
+//
+
+import SwiftUI
+
+@main
+struct WakeUpCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
